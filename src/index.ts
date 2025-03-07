80020d1373e55d23276ac73f20ace76acebcc2b1
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2025-03-06 21:22:08 -0300

    Inserir aluno

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,21 @@ async function main() {
 
   const connection = pgp()(connectionString);
   try {
+    // Usando o scanner, vamos obter os dados do aluno para inserir
+    // ENTRADA
+    console.log("A seguir, informe os dados do aluno: \n");
+    const nome = await scanner.question("Nome Completo: ");
+    const dataNascimento = await scanner.question(
+      "Data Nascimento (ex: 12/11/1990): "
+    );
+    const cpf = await scanner.question("CPF: ");
+    const telefone = await scanner.question("Telefone: ");
+    const sexo = await scanner.question("Sexo (M-Masculino/F-Feminino): ");
+    const email = await scanner.question("Email: ");
+    const escolaridade = await scanner.question("Nível Escolaridade: ");
+    const renda = await scanner.questionFloat("Renda: ");
+    const pcd = await scanner.question("É PCD? (S-Sim/N-Não): ");
+
     // Monta a query de inserção
     const queryInsertAlunos = `
       insert into alunos (nome, data_nascimento, cpf,
@@ -17,19 +32,24 @@ async function main() {
       values ($1, $2, $3, $4, $5, $6, $7, $8, $9);
     `;
 
+    // PROCESSAMENTO
+
+    const [dia, mes, ano] = dataNascimento.split("/");
+    const dataNascimentoISO = new Date(`${mes}/${dia}/${ano}`).toUTCString();
     // Insere os dados no banco
     await connection.query(queryInsertAlunos, [
-      "Manezinho da Ilha",
-      "1990-04-20",
-      "12345678903",
-      "47999990420",
-      "M",
-      "omanezinho@hotmail.com",
-      "ensino médio",
-      1518.0,
-      false,
+      nome,
+      dataNascimentoISO,
+      cpf,
+      telefone,
+      sexo.charAt(0),
+      email,
+      escolaridade,
+      renda,
+      pcd.toUpperCase() === "S",
     ]);
 
+    // SAÍDA
     // Busca os dados no banco
     const results = await connection.query("select id, nome, cpf from alunos");
 
@@ -37,7 +57,7 @@ async function main() {
     console.log(results);
   } catch (error) {
     // Imprime o erro
-    console.log(error)
+    console.log(error);
   }
 
   scanner.close();
